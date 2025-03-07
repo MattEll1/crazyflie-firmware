2cddbfaa4cd4ed6ca7609504bf8661453579bd0d
Matthias <matthias.elliot@my.uwi.edu>
2025-03-07 03:31:55 +0000

    Created platform file for imx93

---
+++ src/platform/platform_imx93.c
@@ -0,0 +1,72 @@
+/**
+ *    ||          ____  _ __                           
+ * +------+      / __ )(_) /_______________ _____  ___ 
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2011-2018 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Platform functionality for the IMX93 HITL platform
+ */
+
+#define DEBUG_MODULE "PLATFORM"
+
+#include <string.h>
+
+#include "platform.h"
+#include "exti_imx93.h"
+#include "nvic_imx93.h"
+#include "debug.h"
+
+static platformConfig_t configs[] = {
+  {
+    .deviceType = "IMX93",
+    .deviceTypeName = "UCM-IMX93",
+    .sensorImplementation = SensorImplementation_sim,
+    .physicalLayoutAntennasAreClose = true,
+  }
+  // {
+  //   .deviceType = "CF21",
+  //   .deviceTypeName = "Crazyflie 2.1",
+  //   .sensorImplementation = SensorImplementation_sim,
+  //   .physicalLayoutAntennasAreClose = true,
+  // }
+};
+
+const platformConfig_t* platformGetListOfConfigurations(int* nrOfConfigs) {
+  *nrOfConfigs = sizeof(configs) / sizeof(platformConfig_t);
+  return configs;
+}
+
+void platformInitHardware() {
+  //Low level init: Clock and Interrupt controller
+  nvicInit();
+
+  //EXTI interrupts
+  extiInit();
+
+  DEBUG_PRINT("iMX93 platform initialized\n");
+}
+
+
+// Config functions ------------------------
+
+const char* platformConfigGetPlatformName() {
+  return "imx93";
+}
+
